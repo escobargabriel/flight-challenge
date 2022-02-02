df89a44176d99bc078deba0b23df98f3c9b015d9
Gabriel Escobar <gabrielgbg@gmail.com>
2022-02-02 12:41:43 -0300

    Create GetStream and getInfo methods to make the communication of client with the server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,9 @@
+import java.nio.charset.StandardCharsets;
+
 import org.apache.arrow.flight.FlightClient;
+import org.apache.arrow.flight.FlightDescriptor;
+import org.apache.arrow.flight.FlightInfo;
+import org.apache.arrow.flight.FlightStream;
 import org.apache.arrow.flight.Location;
 import org.apache.arrow.memory.RootAllocator;
 
@@ -13,6 +18,14 @@ public class Client {
         .allocator(allocator)
         .build();
 
-    // Make the call to the getFlightInfo and GetStream
+    final FlightInfo flightInfo =
+        client.getInfo(FlightDescriptor.command("getInfo".getBytes(StandardCharsets.UTF_8)));
+
+    final FlightStream flightStream = client.getStream(flightInfo.getEndpoints().get(0).getTicket());
+
+    while (flightStream.next()) {
+      System.out.println(flightStream.getRoot().getFieldVectors());
+    }
   }
+
 }
